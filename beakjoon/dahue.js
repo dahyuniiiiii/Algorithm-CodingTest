ca06b6388af862150cf5d6a1d66964e03de7b22a
dahyuniiiiii <imcute0703123@naver.com>
2025-09-05 11:02:49 +0900

    2025-09-05

--- beakjoon/dahue.js
+++ beakjoon/dahue.js
@@ -1,16 +1,11 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
-
-const al = input[1];
-let count = 0;
-for(let i=0; i<al.length; i++){
-    if(al[i]=='O'){
-        count+=1;
-    }
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+let result = [];
+function sum(start) {
+  if (start > 5) return;
+  result.push(start);
+  sum(start + 1);
 }
-if (count>Math.floor(al.length/2)){
-    console.log('Yes');
-}else{
-    console.log('No');
-}
+sum(1);
+console.log(result.join(" "));
