30579cc22117905c3dbb79c60c4f2867e324313a
dahyuniiiiii <imcute0703123@naver.com>
2025-10-02 14:32:43 +0900

    2025-10-02

---
+++ beakjoon/5086.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+
+  if (a === 0 && b === 0) break; 
+
+  if (b % a === 0) {
+    console.log("factor");
+  } else if (a % b === 0) {
+    console.log("multiple");
+  } else {
+    console.log("neither");
+  }
+}
