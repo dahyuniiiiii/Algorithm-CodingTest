dd42622e28b3c6bd822defeec9d40526bcbe7cce
dahyuniiiiii <imcute0703123@naver.com>
2025-10-19 18:03:35 +0900

    2025-10-19

---
+++ beakjoon/24264.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+const n = input[0];
+console.log(n**2);
+console.log(2);
