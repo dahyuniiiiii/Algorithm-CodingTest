723969faf72b66e7f5192fc796490f3dece87694
dahyuniiiiii <imcute0703123@naver.com>
2025-10-21 01:37:18 +0900

    2025-10-21

--- beakjoon/24264.js
+++ beakjoon/24264.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n")
-  .map(Number);
-const n = input[0];
-console.log(n**2);
+const n = Number(fs.readFileSync(filePath).toString().trim());
+
+// 수행 횟수: 1 + 2 + ... + (n - 1) = n(n - 1) / 2
+console.log((n * (n - 1)) / 2);
 console.log(2);
