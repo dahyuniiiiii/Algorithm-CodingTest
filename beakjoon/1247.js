ad934f8c53f24cca08e4c7eb448fbe0592a64e25
dahyuniiiiii <imcute0703123@naver.com>
2025-11-03 15:59:43 +0900

    2025-11-01

---
+++ beakjoon/1247.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let num = 0;
+
+while (num < input.length) {
+  const n = parseInt(input[num++]);
+  let result = 0n;
+
+  for (let i = 0; i < n; i++) {
+    result += BigInt(input[num++]);
+  }
+
+  if (result > 0n) {
+    console.log('+');
+  } else if (result === 0n) {
+    console.log('0');
+  } else {
+    console.log('-');
+  }
+}
