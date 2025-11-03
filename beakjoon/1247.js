e6b30524b20f3cd2c331ac3da14274c69e0056ac
dahyuniiiiii <imcute0703123@naver.com>
2025-11-03 16:00:22 +0900

    2025-11-03

--- beakjoon/1247.js
+++ beakjoon/1247.js
@@ -7,11 +7,9 @@ let num = 0;
 while (num < input.length) {
   const n = parseInt(input[num++]);
   let result = 0n;
-
   for (let i = 0; i < n; i++) {
     result += BigInt(input[num++]);
   }
-
   if (result > 0n) {
     console.log('+');
   } else if (result === 0n) {
