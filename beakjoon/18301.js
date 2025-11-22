134043ab09c51e537d172bf4a46fc9358d3bb986
dahyuniiiiii <imcute0703123@naver.com>
2025-11-23 00:10:49 +0900

    2025-11-23

--- beakjoon/18301.js
+++ beakjoon/18301.js
@@ -3,6 +3,4 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split(" ");
 
 const [a, b, c] = input.map(Number);
-
-const result = ((a + 1) * (b + 1)) / (c + 1) - 1;
-console.log(Math.floor(result));
+console.log(Math.max(b - a, c - b) - 1);
