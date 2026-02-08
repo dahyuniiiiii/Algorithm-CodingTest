785aae3c1fcb16c85d4891ebd4b84b32544a9acd
dahyuniiiiii <imcute0703123@naver.com>
2026-02-08 13:55:07 +0900

    2026-02-08

---
+++ beakjoon/24086.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [a, b] = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+console.log(Math.max(a, b) - Math.min(a, b));
